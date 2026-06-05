ce6701bad55085de6bee8e0a210134efefc3ecb8
RebornAdi <deshmukhaditya105@gmail.com>
2026-06-05 22:34:15 +0530

    Complete structure

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jose import JWTError, jwt
